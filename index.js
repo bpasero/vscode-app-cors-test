7b769c7c0ad3c4a1c3d6e768c3d54fc889583a10
Benjamin Pasero <benjpas@microsoft.com>
2021-02-18 08:13:32 +0100

    port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
